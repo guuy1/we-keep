53756103e7ee58eb8d828a9137e27168f0d002cc
Guy <guuy1@hotmail.com>
2020-01-07 17:20:05 +0200

    firebase context

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
-import App from "./copms/App/App";
+import App from "./copms/App";
+import Firebase, { FirebaseContext } from './components/Firebase';
 
-ReactDOM.render( < App / > , document.getElementById("root"));
+
+ReactDOM.render(
+<FirebaseContext.Provider value={new Firebase()}> 
+< App / > 
+</FirebaseContext.Provider>, document.getElementById("root"));
 
 serviceWorker.unregister();
