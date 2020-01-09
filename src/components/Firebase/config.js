bb78076e480a31852182c409a9f8950d4f4790af
Guy <guuy1@hotmail.com>
2020-01-09 16:00:15 +0200

    change name of components folder

---
+++ src/components/Firebase/config.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBhw3uIjjxR7WhG6c0OQXiG9U75sMO3W7w",
+    authDomain: "house-keeping-186d2.firebaseapp.com",
+    databaseURL: "https://house-keeping-186d2.firebaseio.com",
+    projectId: "house-keeping-186d2",
+    storageBucket: "house-keeping-186d2.appspot.com",
+    messagingSenderId: "1023958152371",
+    appId: "1:1023958152371:web:cef2a5e4218f6971776cc6",
+    measurementId: "G-VQT9GBS4H1"
+  };
+
+  export {firebaseConfig};
